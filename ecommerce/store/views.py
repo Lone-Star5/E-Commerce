8c8f48c227c31608bcc0a9d85b7facb5c6f6e188
Lone-Star5 <amankap05@gmail.com>
2020-07-17 11:04:33 +0530

    added all the templates and views and urls

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	return render(request,'store/main.html')
+
+def store(request):
+	return render(request,'store/store.html')
+
+def cart(request):
+	return render(request,'store/cart.html')
+
+def checkout(request):
+	return render(request,'store/checkout.html')
