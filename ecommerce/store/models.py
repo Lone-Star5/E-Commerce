19be50f856789e7ba27a8214bf074023d6e4d1ba
Lone-Star5 <amankap05@gmail.com>
2020-07-17 15:35:02 +0530

    added order items to cart and checkout page and connected the backend with frontend

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -38,11 +38,31 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+	@property
+	def getCartTotal(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.getTotal for item in orderitems])
+		return total
+
+	@property
+	def getCartQuantity(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total
+
+	
+
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product,on_delete=models.SET_NULL, null = True, blank=True)
 	order = models.ForeignKey(Order,on_delete=models.SET_NULL, null = True, blank=True)
 	quantity = models.IntegerField(default=0,null=True,blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+	@property
+	def getTotal(self):
+		total = self.product.price*self.quantity;
+		return total
+	
 
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, null = True, blank=True)
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -11,7 +11,23 @@ def store(request):
 	return render(request,'store/store.html',{'products':products})
 
 def cart(request):
-	return render(request,'store/cart.html')
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order,created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
+		order = {'getCartTotal':0, 'getCartQuantity':0}
+	context = {'items':items, 'order':order}
+	return render(request,'store/cart.html',context)
 
 def checkout(request):
-	return render(request,'store/checkout.html')
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order,created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
+		order = {'getCartTotal':0, 'getCartQuantity':0}
+	context = {'items':items, 'order':order}
+	return render(request,'store/checkout.html',context)
